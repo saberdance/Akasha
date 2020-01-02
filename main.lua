9f1a94e2f93f20e13a1e50a5d694614d592c23af
Ubuntu <ubuntu@ip-172-31-36-246.cn-northwest-1.compute.internal>
2020-01-02 08:53:11 +0000

    Test MySQL

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
-socket=require("socket")
+local socket=require("socket")
 print(socket._VERSION)
-sql=require("luasql")
+local sql=require("luasql.mysql")
+local env=assert(sql.mysql())
+local conn=assert(env:connect("cot2","root","]cotnetwork}","localhost",3306))
+local cursor,errString=conn:execute([[select * from lumion_upfiles]])
+local row=cursor:fetch({},"a")
+while row do
+	print(row.fileName)
+	row=cursor:fetch(row,"a")
+end
